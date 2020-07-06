1445d49eb38e9a9d1ff57032829e98710e79cc38
Ludmila Zhylianova <ludiky30@gmail.com>
2020-07-07 02:07:03 +0700

    lesson 19 hard

--- js/script.js
+++ js/script.js
@@ -130,5 +130,33 @@ window.addEventListener("DOMContentLoaded", function() {
 
   togglePopUp();
 
+  //плавная прокрутка
+
+  const scrolling = () => {
+    const menuItems = document.querySelectorAll("menu>ul>li>a");
+    const downBtn = document.querySelector("main>a[href=\"#service-block\"]");
+
+    const smoothScroll = (section, speed) => {
+      if (document.documentElement.scrollTop < section.offsetTop) {
+        scrollBy(0,speed);
+        requestAnimationFrame(() => smoothScroll(section, speed));
+      }
+    }
+
+    menuItems.forEach((elem, key) => {
+      elem.addEventListener("click", event => {
+        event.preventDefault();
+        smoothScroll(document.querySelector(elem.getAttribute("href")), (key + 1) * 20);
+      });
+    });
+
+    downBtn.addEventListener("click", event => {
+      event.preventDefault();
+      smoothScroll(document.querySelector(downBtn.getAttribute("href")), 20);
+    });
+    
+  }
+
+  scrolling();
 
 });

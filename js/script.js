f19eac8d7bd08f8f07dc1630a3900325a3ab68d9
Ludmila Zhylianova <ludiky30@gmail.com>
2020-07-16 21:12:48 +0700

    lesson 23

--- js/script.js
+++ js/script.js
@@ -355,14 +355,57 @@ window.addEventListener("DOMContentLoaded", function() {
     
     calc.addEventListener("input", (event) => {
       const target = event.target;
-      if (target.classList.contains("calc-item")) {
+      if (target.matches("input.calc-item")) {
         const value = target.value
         target.value = value.replace(/\D/,"");
- 
       }
     })
 
   }
   calculatVerification();
+
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector(".calc-block");
+    const calcType = document.querySelector(".calc-type");
+    const calcSquare = document.querySelector(".calc-square");
+    const calcDay = document.querySelector(".calc-day");
+    const calcCount = document.querySelector(".calc-count");
+    const totalValue = document.getElementById("total");
+
+    const countSum = () => {
+      let total = 0;
+      let countValue = 1;
+      let dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value;
+      const sqareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5
+      }
+
+      if (typeValue && sqareValue) {
+         total = price * typeValue * sqareValue * countValue * dayValue;
+       } 
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener("change",(event) => {
+      const target = event.target;
+
+      if (target.matches("select") || target.matches("input")) {
+        countSum();
+      }
+
+    })
+  };
+
+  calc(100);
 
 });
